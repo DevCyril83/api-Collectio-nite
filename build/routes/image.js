e0419b9cbe5d762d2019abc902e71c5211915c77
cyril <cyrilha13@gmail.com>
2024-08-08 18:11:42 +0200

    consolidation des routes de image

--- build/routes/image.js
+++ build/routes/image.js
@@ -10,8 +10,16 @@ const User = require("../databases/User");
 exports.imageRouter.get("/:id", async (request, reponse) => {
     const imageId = request.params.id;
     const image = await Image.findByPk(imageId)
-        .catch(error => { console.log(error); });
-    reponse.status(200).json(image);
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
+    if (image) {
+        reponse.status(200).json(image);
+    }
+    else {
+        reponse.status(400).json("an error has occured");
+    }
 });
 exports.imageRouter.post("/", async (request, reponse) => {
     const imageBody = request.body;
@@ -20,52 +28,123 @@ exports.imageRouter.post("/", async (request, reponse) => {
         description: imageBody.url,
         auteur: imageBody.auteur
     })
-        .catch(error => { console.log(error); });
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
     reponse.status(200).json(image);
 });
 exports.imageRouter.post("/user", async (request, reponse) => {
     const body = request.body;
-    console.log(body);
-    const user = await User.findByPk(body.userId);
-    console.log(user);
-    const image = await Image.findByPk(body.imageId);
-    console.log(image);
-    await image.addUser(user);
+    const user = await User.findByPk(body.userId)
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
+    const image = await Image.findByPk(body.imageId)
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
+    await image.addUser(user)
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
     reponse.status(200).json("image has been add to user");
 });
 exports.imageRouter.get("/:userId", async (request, reponse) => {
     const userId = request.params.userId;
     const user = await User.findByPk(userId)
-        .catch(error => { console.log(error); });
-    const image = await user.getImages;
-    reponse.status(200).json(image);
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
+    const image = await user.getImages
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
+    if (image) {
+        reponse.status(200).json(image);
+    }
+    else {
+        reponse.status(404).json("no image");
+    }
 });
 exports.imageRouter.post("/item", async (request, reponse) => {
     const body = request.body;
-    const item = await Item.findByPk(body.itemId);
-    const image = await Image.findByPk(body.imageId);
-    console.log(image);
-    await image.addItem(item);
+    const item = await Item.findByPk(body.itemId)
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
+    const image = await Image.findByPk(body.imageId)
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
+    await image.addItem(item)
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
     reponse.status(200).json("image has been add to item");
 });
 exports.imageRouter.get("/item/:itemId", async (request, reponse) => {
     const itemId = request.params.itemId;
     const item = await Item.findByPk(itemId)
-        .catch(error => { console.log(error); });
-    const image = await item.getImages();
-    reponse.status(200).json(image);
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
+    const image = await item.getImages()
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
+    if (image) {
+        reponse.status(200).json(image);
+    }
+    else {
+        reponse.status(400).json("no image");
+    }
 });
 exports.imageRouter.post("/collection", async (request, reponse) => {
     const body = request.body;
-    const collection = await Collection.findByPk(body.collectionId);
-    const image = await Image.findByPk(body.imageId);
-    await image.addCollection(collection);
+    const collection = await Collection.findByPk(body.collectionId)
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
+    const image = await Image.findByPk(body.imageId)
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
+    await image.addCollection(collection)
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
     reponse.status(200).json("image has been add to collection");
 });
 exports.imageRouter.get("/collection/:collectionId", async (request, reponse) => {
     const collectionId = request.params.collectionId;
     const collection = await Collection.findByPk(collectionId)
-        .catch(error => { console.log(error); });
-    const image = await Image.findByPk(collection.ImageId);
-    reponse.status(200).json(image);
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
+    const image = await Image.findByPk(collection.ImageId)
+        .catch(error => {
+        console.log(error);
+        reponse.status(500).json("an error has occured");
+    });
+    if (image) {
+        reponse.status(200).json(image);
+    }
+    else {
+        reponse.status(400).json("no image");
+    }
 });
--- src/routes/image.ts
+++ src/routes/image.ts
@@ -15,9 +15,16 @@ imageRouter.get("/:id", async (request,reponse)=>{
     const imageId = request.params.id;
 
     const image = await Image.findByPk(imageId)
-    .catch(error=>{console.log(error)});
-   
-    reponse.status(200).json(image);
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
+
+    if(image){
+        reponse.status(200).json(image);
+    }else{
+        reponse.status(400).json("an error has occured");
+    }
 });
 
 
@@ -29,7 +36,10 @@ imageRouter.post("/", async (request,reponse)=>{
         description : imageBody.url,
         auteur : imageBody.auteur
     })
-    .catch(error=>{console.log(error)})
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
 
     reponse.status(200).json(image)
 });
@@ -37,12 +47,24 @@ imageRouter.post("/", async (request,reponse)=>{
 
 imageRouter.post("/user", async (request,reponse)=>{
     const body = request.body;
-    console.log(body)
-    const user = await User.findByPk(body.userId);
-    console.log(user)
-    const image = await Image.findByPk(body.imageId);
-    console.log(image)
-    await image.addUser(user);
+ 
+    const user = await User.findByPk(body.userId)
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
+
+    const image = await Image.findByPk(body.imageId)
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
+
+    await image.addUser(user)
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
 
     reponse.status(200).json("image has been add to user")
 });
@@ -52,46 +74,113 @@ imageRouter.get("/:userId", async (request,reponse)=>{
     const userId = request.params.userId;
 
     const user = await User.findByPk(userId)
-    .catch(error =>{console.log(error)});
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
 
     const image = await user.getImages
-    reponse.status(200).json(image);
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
+    if(image){
+        reponse.status(200).json(image);
+    }else{
+        reponse.status(404).json("no image")
+    }
 });
 
 
 imageRouter.post("/item", async (request, reponse) => {
     const body = request.body;
-    const item = await Item.findByPk(body.itemId);
-    const image = await Image.findByPk(body.imageId);
-    console.log(image);
-    await image.addItem(item);
+
+    const item = await Item.findByPk(body.itemId)
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
+
+    const image = await Image.findByPk(body.imageId)
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
+    
+    await image.addItem(item)
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
+
     reponse.status(200).json("image has been add to item");
 });
 
 
 imageRouter.get("/item/:itemId", async (request, reponse) => {
     const itemId = request.params.itemId;
+    
     const item = await Item.findByPk(itemId)
-        .catch(error => { console.log(error); });
-    const image = await item.getImages();
-    reponse.status(200).json(image);
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
+
+    const image = await item.getImages()
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
+    if(image){
+        reponse.status(200).json(image);
+    }else{
+        reponse.status(400).json("no image")
+    }
 });
 
 
 imageRouter.post("/collection", async (request, reponse) => {
     const body = request.body;
-    const collection = await Collection.findByPk(body.collectionId);
-    const image = await Image.findByPk(body.imageId);
-    await image.addCollection(collection);
+
+    const collection = await Collection.findByPk(body.collectionId)
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
+
+    const image = await Image.findByPk(body.imageId)
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
+
+    await image.addCollection(collection)
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
     reponse.status(200).json("image has been add to collection");
 });
 
 
 imageRouter.get("/collection/:collectionId", async (request, reponse) => {
     const collectionId = request.params.collectionId;
+    
     const collection = await Collection.findByPk(collectionId)
-        .catch(error => { console.log(error); });
-    const image = await Image.findByPk(collection.ImageId);
-    reponse.status(200).json(image);
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
+
+    const image = await Image.findByPk(collection.ImageId)
+    .catch(error=>{
+        console.log(error)
+        reponse.status(500).json("an error has occured")
+    });
+    if(image){
+        reponse.status(200).json(image);
+    }else{
+        reponse.status(400).json("no image")
+    }   
 });
 
